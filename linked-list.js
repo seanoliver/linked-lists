57fde4c97b868ac0624a584afb6bf0d94dcc3c51
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-29 12:05:21 -0700

    added push unshift and pop implementations for linked lists

---
+++ linked-list.js
@@ -0,0 +1,102 @@
+/** Node: node for a singly linked list. */
+
+class Node {
+	val = null;
+	next = null;
+
+	constructor(val) {
+		this.val = val;
+	}
+}
+
+/** LinkedList: chained together nodes. */
+
+class LinkedList {
+	head = null;
+	tail = null;
+	length = 0;
+
+	constructor(vals = []) {
+		for (let val of vals) this.push(val);
+	}
+
+	/** push(val): add new value to end of list. */
+
+	push(val) {
+		const newNode = new Node(val);
+
+		if (this.head === null) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+
+		this.length++;
+		return undefined;
+	}
+
+	/** unshift(val): add new value to start of list. */
+
+	unshift(val) {
+		const newNode = new Node(val);
+
+		if (this.head === null) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+
+		this.length++;
+	}
+
+	/** pop(): return & remove last item. */
+
+	pop() {
+    if (this.head === null) throw new Error('Empty list!');
+
+    let current = this.head;
+    for (let i = 0; i < this.length; i++) {
+      if (current.next === this.tail) {
+        break;
+      }
+      current = current.next;
+    }
+
+    const oldTail = this.tail;
+    current.next = null;
+    this.tail = current;
+    this.length--;
+
+    return oldTail;
+  }
+
+	/** shift(): return & remove first item. */
+
+	shift() {}
+
+	/** getAt(idx): get val at idx. */
+
+	getAt(idx) {}
+
+	/** setAt(idx, val): set val at idx to val */
+
+	setAt(idx, val) {}
+
+	/** insertAt(idx, val): add node w/val before idx. */
+
+	insertAt(idx, val) {}
+
+	/** removeAt(idx): return & remove item at idx, */
+
+	removeAt(idx) {}
+
+	/** average(): return an average of all values in the list */
+
+	average() {}
+}
+
+module.exports = LinkedList;
